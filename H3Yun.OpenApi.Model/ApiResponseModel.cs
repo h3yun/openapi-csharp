bfb4ce545dc5fe954224af77e7cbb9c479fa35c3
caojp <caojp@authine.com>
2023-01-17 12:09:33 +0800

    增加通用数据模型项目

---
+++ H3Yun.OpenApi.Model/ApiResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace H3Yun.OpenApi.Model
+{
+    public class ApiResponseModel<T>
+    {
+        public bool Successful { get; set; }
+        public string ErrorMessage { get; set; }
+        public bool Logined { get; set; }
+        public T Data { get; set; }
+    }
+}
---
+++ H3Yun.OpenApi.Model/BizObjectModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace H3Yun.OpenApi.Model
+{
+    public class BizObjectModel
+    {
+        public Dictionary<string, object> BizObject { get; set; }
+    }
+}
